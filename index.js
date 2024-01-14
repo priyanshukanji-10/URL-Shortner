5d8dd92c613b6ff0e8387308fae41812e1c1376d
Priyanshu <priyanshukanji0@gmail.com>
2024-01-14 13:23:04 +0530

    Css added for home page

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
 const express = require("express");
 const path = require("path");
 
-// Imporing Routers
+// Importing Routers
 const urlRouter = require("./routes/url");
 const redirectRouter = require("./routes/redirect");
 const staticRoute = require("./routes/staticRoute");
-// Creating apps
+
+// Creating app
 const app = express();
+
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// Setting up to Server Side Rendering using EJS
+
+// Setting up for Server-Side Rendering using EJS
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
+
+// Serving static files from the 'public' directory
+app.use(express.static('public'));
+
 // Connecting to MongoDB
 const { connectMongoDB } = require("./connect");
-connectMongoDB("mongodb://localhost:27017/short-url").then(() =>
-  console.log("Mongodb connected")
-);
+connectMongoDB("mongodb://localhost:27017/short-url")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 // Routers
-app.use("/", staticRoute);
 app.use("/url", urlRouter);
 app.use("/", redirectRouter);
- 
+app.use("/", staticRoute);
 
-const PORT = 5001;
-app.listen(PORT, () => console.log(`Server started at Port:${PORT}`));
+// Define a catch-all route for any unmatched routes
+app.use((req, res) => {
+  res.status(404).send("Page not found");
+});
+
+// Start the server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server started at Port: ${PORT}`));
